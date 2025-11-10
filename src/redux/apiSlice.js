a7efb141641bb9e1da3e3f38d5eac05244baaecb
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-11-10 20:27:50 +0600

    Updated

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { logout, setCredentials } from "./features/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://nextjs-food-ordering-server.vercel.app",
+  baseUrl: "https://nextjs-food-ordering-server.vercel.app/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.accessToken;
