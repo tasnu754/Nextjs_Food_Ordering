72a72ba897f88341fb4c2e1e43d592cbe8389f57
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-11-10 20:02:34 +0600

    Updated

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { logout, setCredentials } from "./features/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.SERVER_BASE_API_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.accessToken;
